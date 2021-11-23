174766ba8d29d93cbfee8865edd9f61497915c82
yxoni <90109410+yxoni@users.noreply.github.com>
2021-11-23 23:47:00 +0900

    Update hw7.c

--- hw7.c
+++ hw7.c
@@ -11,14 +11,12 @@ int main(void) {
     printf("Enter 5 real numbers : ");
     scanf_s("%lf %lf %lf %lf %lf", &num[0], &num[1], &num[2], &num[3], &num[4]);
 
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         sum = sum + num[i];
     }
     avg = sum / 5;
     sum = 0;
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         double square = pow(num[i] - avg, 2);
         sum = sum + square;
     }
